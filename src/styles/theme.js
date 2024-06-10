2fba4f28a5739f3dbd5f674a504cf17de8506474
Igor Almeida <tioban.alm@gmail.com>
2024-06-10 02:09:37 +0000

    feat: instaling and setting up react-router-dom and styled-components :D

---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+    homeBg: '#131518',
+    cardGradient: 'radial-gradient(98.96% 98.96% at 50% 0%, #232a34 0%, #181e27 100%)',
+    txtColor: '#969fad',
+    submitColor: '#fc7614',
+    white: '#fff',
+    feedbackColor: '#262e38',
+    feedbacktext: '#7c8798'
+}
