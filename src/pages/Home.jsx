6471d00bd6ac41596243f7658f1afb4959ee0906
Igor Almeida <tioban.alm@gmail.com>
2024-06-10 12:04:18 +0000

    feat: note and submit verifycation added!! (project completed)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,23 @@ import { ButtonContainer, CardContainer, IconContainer, ResultContainer } from '
 
 import iconStar from '../assets/icon-star.svg'
 import thanks from '../assets/illustration-thank-you.svg'
+import { useState } from 'react'
 
 export function Home() {
-  let showResults = true
+  const [showResults, setShowResults] = useState(false)
+  const [feedbackNote, setFeedbackNote] = useState(0)
+
+  function handleFeedbackButtonClick(event) {
+    const feedback = Number(event.target.innerText)
+
+    setFeedbackNote(feedback)
+  }
+
+  function handleSubmit() {
+    if (feedbackNote === 0) return
+
+    setShowResults(true)
+  }
 
   return (
     showResults === false ? (
@@ -18,14 +32,14 @@ export function Home() {
         <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
 
         <ButtonContainer>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
+          <button onClick={handleFeedbackButtonClick}>1</button>
+          <button onClick={handleFeedbackButtonClick}>2</button>
+          <button onClick={handleFeedbackButtonClick}>3</button>
+          <button onClick={handleFeedbackButtonClick}>4</button>
+          <button onClick={handleFeedbackButtonClick}>5</button>
         </ButtonContainer>
 
-        <button>Enviar</button>
+        <button onClick={handleSubmit}>Enviar</button>
         
       </CardContainer>
     ) : (
@@ -33,7 +47,7 @@ export function Home() {
         <img src={thanks} alt="imagem de agradecimento" />
 
         <ResultContainer>
-          <p>voce selecionou 4 de 5</p>
+          <p>voce selecionou {feedbackNote} de 5</p>
         </ResultContainer>
 
         <h1>Obrigado!</h1>
